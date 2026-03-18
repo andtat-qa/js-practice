b4040b48eb08ff2c323b2a1c63e972bc7dab8a7b
Andrii Tataurov <andtat@qamadness.com>
2026-03-18 18:46:07 +0200

    Add lesson4 tasks

---
+++ lesson4/task1.js
@@ -0,0 +1,9 @@
+// Variable to check
+let number = Math.floor(Math.random() * 21)+1;
+
+// Check if even or odd
+if (number % 2 === 0) {
+    console.log("Number " + number + " is even");
+} else {
+    console.log("Number " + number + " is odd");
+}
