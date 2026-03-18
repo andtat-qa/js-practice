6786f2c1f9e97b9b1008b8d2f8eebdb307b18026
Andrii Tataurov <andtat@qamadness.com>
2026-03-18 17:41:15 +0200

    Add lesson3 tasks

---
+++ lesson3/task2.js
@@ -0,0 +1,7 @@
+// Variables
+const age = Math.floor(Math.random() * 21) + 10; // Random value between 10 and 30
+const isAdult = age >= 18; // Boolean check for adulthood
+
+// Output
+console.log("Age:", age);
+console.log("Is adult?", isAdult);
