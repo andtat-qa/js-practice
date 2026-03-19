2ca3c6585b2f1961fc63d93565239f00c090278a
Andrii Tataurov <andtat@qamadness.com>
2026-03-19 15:49:58 +0200

    Add lesson8 tasks

---
+++ lesson8/task2.js
@@ -0,0 +1,9 @@
+// Array of colors
+const colors = ["red", "green", "blue", "yellow"];
+
+// Destructuring the first two elements
+const [color1, color2] = colors;
+
+// Output the extracted values
+console.log(color1); // red
+console.log(color2); // green
